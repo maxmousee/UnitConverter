1f96640bd00153357b11e233c4af1fecc8f18000
Natan Facchin <maxmousee@gmail.com>
2017-05-27 20:14:08 -0300

    Added base project

---
+++ iOS/Unit Converter/Unit Converter/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Unit Converter
+//
+//  Created by Natan Facchin on 27/05/17.
+//  Copyright © 2017 NFS Industries. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
