9e0118af0e8a923ea33f30886b3a7fead61f56fe
Natan Facchin <maxmousee@gmail.com>
2017-05-28 18:28:58 -0300

    Added converter model

---
+++ iOS/Unit Converter/Unit Converter/ConverterModel.swift
@@ -0,0 +1,16 @@
+//
+//  ConverterModel.swift
+//  Unit Converter
+//
+//  Created by Natan Facchin on 28/05/17.
+//  Copyright © 2017 NFS Industries. All rights reserved.
+//
+
+import Foundation
+
+var type: String?
+var availableUnits: String?
+var fromUnit: String?
+var toUnit: String?
+var fromValue: Double?
+var toValue: Double?
